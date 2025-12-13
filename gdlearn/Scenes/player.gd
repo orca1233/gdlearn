d68e4d84b7a4b884aabaed762b359e1b5fb67be5
orca1233 <firebidan@gmail.com>
2025-12-14 03:33:30 +0900

    https://youtu.be/8CrFk3tjsSY?si=5LBNTL21nPa8vG_v 실습(2/9)

--- gdlearn/Scenes/player.gd
+++ gdlearn/Scenes/player.gd
@@ -1,11 +1,13 @@
 extends Node2D
+@export var speed:= 500.0
 # 유형 이름과 일치
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	position = Vector2(100, 100)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = Vector2(100, 200)
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed * delta 
