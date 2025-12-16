44f1b0a925e571dec531c4be8762f11a7b5719d7
orca1233 <firebidan@gmail.com>
2025-12-17 03:51:17 +0900

    플레이어 이동 // 저속이동까지 구현

--- danmaku-minus-5-sexdead/Assets/Scenes/player.gd
+++ danmaku-minus-5-sexdead/Assets/Scenes/player.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
-@export var speed:= 500.0
+
+@export var speed: float = 300.0
 @export var focus_speed: float = 150.0
+@onready var hitbox_sprite = $HitboxSprite # 히트박스 표시용 스프라이트
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	position = Vector2(576, 600)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _physics_process(_delta):
+	# 1. 입력 방향
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	# 디버깅용
+	# print(direction)
+	
+	# 2. 저속 모드 체크
+	var current_speed = speed
+	if Input.is_action_pressed("shift"):
+		current_speed = focus_speed
+		hitbox_sprite.visible = true
+	else:
+		hitbox_sprite.visible = false
+
+	# 3. 이동 적용
+	velocity = direction * current_speed
 	move_and_slide()
