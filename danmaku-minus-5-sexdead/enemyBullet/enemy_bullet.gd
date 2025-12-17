4c38d636195c987d63a01410a83bd491b90038a2
dddops <Eopseum0@gmail.com>
2025-12-18 06:33:35 +0900

    boss 2일차

--- danmaku-minus-5-sexdead/enemyBullet/enemy_bullet.gd
+++ danmaku-minus-5-sexdead/enemyBullet/enemy_bullet.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
-var speed = 600
-var player_pos
-var can_shoot = true
-var move_to
-@onready var player = get_tree().get_first_node_in_group("player") # player 가져오기
+var speed = 100
+var direction = Vector2.RIGHT
+	
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
+	
+func _on_screen_exited():
+	queue_free()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
