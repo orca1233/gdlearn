c694cac4b4f1b0206a9517c13f1ec533b9fe206a
dddops <Eopseum0@gmail.com>
2025-12-18 07:12:54 +0900

    2일파 보스 패턴

--- danmaku-minus-5-sexdead/boss/boss_1.gd
+++ danmaku-minus-5-sexdead/boss/boss_1.gd
@@ -11,15 +11,14 @@ func get_vector(angle):
 
 func shoot(angle):
 	var bullet = bullet_node.instantiate()
+	get_tree().current_scene.call_deferred("add_child", bullet)
 	
 	bullet.position = global_position # 총알이 보스에 따라 움직이면 안되니
 	bullet.direction = get_vector(angle)
-	
-	get_tree().current_scene.call_deferred("add_child", bullet)
+
 # get_tree().currnet_scene = 지금 실행 중인 게임의 바탕이 되는 씬을 가져오고
 # call_defered("add_child", bullet) = 신호나 물리 연산뒤에 총알 생성
 # 그냥 안하는 이유는 검색 ㄱ
-
 
 func _on_timer_timeout() -> void:
 	shoot(theta)
--- danmaku-minus-5-sexdead/enemyBullet/enemy_bullet.gd
+++ danmaku-minus-5-sexdead/enemyBullet/enemy_bullet.gd
@@ -1,14 +1,10 @@
 extends Node2D
 
-var speed = 100
+var speed = 300
 var direction = Vector2.RIGHT
 	
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
-	
-func _on_screen_exited():
-	queue_free()
-
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
