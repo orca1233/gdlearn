7c71c4c386464425f8148e1dc1d3c66fa8b4a565
SH <77217128+Slon549@users.noreply.github.com>
2025-12-30 21:31:08 +0900

    수정본

--- danmaku-minus-5-sexdead/scripts/player.gd
+++ danmaku-minus-5-sexdead/scripts/player.gd
@@ -1,18 +1,19 @@
-extends Node
+extends KinematicBody2D
 
-# 속성을 정의합니다.
-var health: int = 100
-var speed: float = 200.0
+var speed = 400
+var velocity = Vector2.ZERO
 
-# 노드가 준비되면 호출됩니다.
-func _ready():
-    pass
-
-# 사용자 입력을 처리하는 함수입니다.
-func _process(delta: float) -> void:
+func _process(delta):
+    velocity = Vector2.ZERO
     if Input.is_action_pressed('ui_right'):
-        position.x += speed * delta
+        velocity.x += 1
     if Input.is_action_pressed('ui_left'):
-        position.x -= speed * delta
+        velocity.x -= 1
+    if Input.is_action_pressed('ui_down'):
+        velocity.y += 1
+    if Input.is_action_pressed('ui_up'):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
+    move_and_slide(velocity)
 
 # 수정본 추가해줘 (디스코드 메시지)
