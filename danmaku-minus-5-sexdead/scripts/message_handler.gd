c0eb342384c0a75083221429199ba8e243237ce5
SH <77217128+Slon549@users.noreply.github.com>
2025-12-30 21:28:08 +0900

    수정본

--- danmaku-minus-5-sexdead/scripts/message_handler.gd
+++ danmaku-minus-5-sexdead/scripts/message_handler.gd
@@ -1,7 +1,7 @@
 extends Node
 
-func _ready():
-    print("Discord message listener ready!")
-
-func on_message_received(message : String):
-    print("Received message: " + message)
+func _on_message_received(message):
+    if message.startswith('!코드'):
+        var code_request = message.substr(6)
+        # 요청된 코드에 대한 처리 로직 추가
+        pass
