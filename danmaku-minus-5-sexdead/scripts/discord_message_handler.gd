a0ba15cb5d070ebd64153a771d858960f264d0f6
SH <77217128+Slon549@users.noreply.github.com>
2025-12-30 21:31:50 +0900

    수정본

--- danmaku-minus-5-sexdead/scripts/discord_message_handler.gd
+++ danmaku-minus-5-sexdead/scripts/discord_message_handler.gd
@@ -3,8 +3,12 @@ extends Node
 func _ready():
     pass
 
-func handle_discord_message(message: String):
-    if message.begins_with("!코드 "):
-        var code = message.substr(5)
-        print("Code received: " + code)
-        // Handle the code execution or storage here
+func handle_message(message: String):
+    if message == "!코드 새로운 파일 생성해줘":
+        create_new_file()  
+
+func create_new_file():
+    var file = File.new()
+    file.open("res://danmaku-minus-5-sexdead/scripts/new_script.gd", File.WRITE)
+    file.store_line("extends Node\n\nfunc _ready():\n    pass")
+    file.close()
